4a0bc62609cdcdd4eb6a0e00059d2a6a0fd2134e
Demina-Yuliya <zlaya72ru@mail.ru>
2024-09-23 16:58:37 +0300

    Work3

---
+++ script.js
@@ -0,0 +1,44 @@
+let a = 10;
+alert(a);
+a = alert ('20');
+
+let year = 2007;
+alert(year);
+
+let creator = ('Brendan Eich');
+alert(creator);
+
+let x = 10;
+let y = 2;
+alert (x+y);
+alert (x-y);
+alert (x*y);
+alert (x/y);
+
+let result = y**5;
+alert(result);
+
+let c = 9;
+let d = 2;
+alert (c % d);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert (num);
+
+let age = Number (prompt('Сколько вам лет?'));
+alert (age);
+
+const user = {
+    name: 'Админ',
+    age: 25,
+    "isAdmin": true
+};
+
+const name = prompt('Введите ваше имя:');
+alert(`Привет, ${name}!`);
